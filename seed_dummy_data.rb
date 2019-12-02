28aa74a0c54fe4c85029ef94e4116f2ef9c55e39
deniseh <deniseddi@hotmail.com>
2019-12-02 15:40:07 +1100

    fix indentation in all views files

--- seed_dummy_data.rb
+++ seed_dummy_data.rb
@@ -1,6 +1,5 @@
 
 
-<% @comments.each do |comment|
-    <p><%= comment.body %><%= comment.pier_id%><p>
-
-    <p><%= comment.body %> by <%= User.find(comment.user_id).email %></p> 
+# <% @comments.each do |comment|
+#     <p><%= comment.body %><%= comment.pier_id%><p>
+#     <p><%= comment.body %> by <%= User.find(comment.user_id).email %></p> 
